158f6d80247bec42c644b5373c9a3c6714a1a65b
Bridget Akellian <akellianbv@gmail.com>
2022-05-23 21:41:20 -0400

    cleaned up code

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+import { CallMissedSharp } from '@material-ui/icons';
 
-function App() {
+const useStyles = makeStyles({
+  primaryButtonColor: {
+    backgroundColor: 'pink'
+  }
+})
+
+const App = () => { //changed to arrow function to keep it up to date with ES6 best practices
+  const classes = useStyles();
+  
   return (
     <div>
       <h1>Hello!</h1>
+      <Button variant="contained" className={ classes.primaryButtonColor }>Primary button</Button>
+      <Button variant="outlined" color="primary">Secondary button</Button>
     </div>
   );
 }
