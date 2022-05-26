744363001727c61a92cf6e19c0a3ff7677ed6810
Bridget Akellian <akellianbv@gmail.com>
2022-05-25 23:30:38 -0400

    creating signup form

--- src/Components/Auth/Signup.jsx
+++ src/Components/Auth/Signup.jsx
@@ -1,9 +1,40 @@
 import React from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      '& > *': {
+        margin: theme.spacing(1),
+        width: '25ch',
+      },
+    },
+  }));
 
 const Signup = () => {
+
+    const classes = useStyles();
+
+    const [userName, setUserName] = React.useState('');
+    
+    const handleChange = (event) => {
+    setUserName(event.target.value);
+
+  };
+    
   return (
     <div>
         <h1>Sign Up</h1>
+        <form className={classes.root} noValidate autoComplete="off">
+        <TextField
+          id="filled-name"
+          label="Username"
+          value={userName}
+          onChange={handleChange}
+          variant="filled"
+        />
+         <input type="submit" value="Sign Up" />
+    </form>
     </div>
 
   )
